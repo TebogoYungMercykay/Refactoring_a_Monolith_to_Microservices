ded7892dab6b450f868ab544b9b6e2b0deb73ea2
Selepe Sello <sbkskhalo.kq@gmail.com>
2022-09-19 05:52:42 +0200

    ALX-Cloud_Development__Project3

---
+++ set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=postgres.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=bucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
+# ALX-Cloud_Development__Project3
